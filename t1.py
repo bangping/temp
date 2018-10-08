e459701e69055e6eb87875b47605dbfc81ca940b
Kuan Yu <yukuan2008@gmail.com>
2018-10-08 14:33:04 +1100

    Update t1.py

--- t1.py
+++ t1.py
@@ -5,23 +5,27 @@ def get_cand(n, arr):
     cur_m = arr[0]
     is_increase = None
     cur_num = 1
+    prev_num = len(arr) + 1
     cand = 0
     for r in arr[1:]:
-        if r == cur_m:
-            cur_num += 1
-        elif is_increase is None:
+        if r != cur_m and is_increase is None:
             is_increase = (r > cur_m)
             cur_num += 1
             cur_m = r
-        elif is_increase == (r > cur_m):
+        elif r != cur_m and is_increase == (r > cur_m):
             cur_num += 1
             cur_m = r
         else:
             cand += int((cur_num + 1) * cur_num / 2)
+            if prev_num <= cur_num:
+                cand += cur_num - prev_num + 1
+            prev_num = cur_num
             cur_num = 1
             cur_m = r
             is_increase = None
     cand += int((cur_num + 1) * cur_num / 2)
+    if prev_num <= cur_num:
+        cand += cur_num - prev_num + 1
     return cand
 
 n = 10
