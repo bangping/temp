f797b70ad40ae8fa67e2f22e46bf85321b54819c
Kuan Yu <yukuan2008@gmail.com>
2018-10-08 13:53:34 +1100

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,34 +1,30 @@
 import os
 import sys
-from collections import defaultdict
 
-def abbr(a, b, al, bl, mem):
-    if (al, bl) in mem:
-        return mem((al. bl))
-    else:
-        r = calc(a, b, al, bl, mem)
-        mem[(al, bl)] = r
-        return r
-
-def calc(a, b, al, bl, mem):
-    if al == 0:
-        return bl == 0
-    elif bl == 0:
-        return a[:al].islower()
-
-    c = a[al - 1]
-    if c.isupper():
-        if c != b[bl - 1]:
-            return False
+def get_cand(n, arr):
+    cur_m = arr[0]
+    is_increase = None
+    cur_num = 1
+    cand = 0
+    for r in arr[1:]:
+        if r == cur_m:
+            cur_num += 1
+        elif is_increase is None:
+            is_increase = (r > cur_m)
+            cur_num += 1
+            cur_m = r
+        elif is_increase == (r > cur_m):
+            cur_num += 1
+            cur_m = r
         else:
-            return abbr(a, b, al - 1, bl - 1, mem)
-    else:
-        uc = c.upper()
-        if uc != b[bl - 1]:
-            return abbr(a, b, al - 1, bl, mem)
-        else:
-            return abbr(a, b, al - 1, bl, mem) or abbr(a, b, al - 1, bl - 1, mem)
+            cand += int((cur_num + 1) * cur_num / 2)
+            cur_num = 1
+            cur_m = r
+            is_increase = None
+    cand += int((cur_num + 1) * cur_num / 2)
+    return cand
 
-def abbreviation(a, b):
-    r = abbr(a, b, len(a), len(b), {})
-    return 'YES' if r else 'NO'
+n = 10
+arr = [2, 4, 2, 6, 1, 7, 8, 9, 2, 1]
+cand = get_cand(n, arr)
+print(str(cand))
